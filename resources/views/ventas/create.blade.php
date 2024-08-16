a09d8354b800d6d6d38224de4d55eebf26a75b54
Alexander <ajvr3023@gmail.com>
2024-08-15 20:01:24 -0500

    Se validó en la vista edit y create para no ingresar productos repetidos

--- resources/views/ventas/create.blade.php
+++ resources/views/ventas/create.blade.php
@@ -102,23 +102,18 @@
             <div>
                 <div id="productos_container">
                     <div class="flex items-center producto_item mb-4">
-                        <select name="id_producto[]"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            required>
+                        <select name="id_producto[]" class="product-select bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                             @foreach($productos as $producto)
                             <option value="{{ $producto->id }}">{{ $producto->descripcion }}</option>
                             @endforeach
                         </select>
-                        <input type="number" name="cantidad[]"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/4 p-2.5 ml-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Cantidad" required pattern="\d*"
-                    oninput="this.value = this.value.replace(/[^0-9]/g, '');" />
-                        <button type="button" class="ml-4 text-red-700 hover:text-red-800"
-                            onclick="removeProduct(this)">Eliminar</button>
+                        <input type="number" name="cantidad[]" class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/4 p-2.5 ml-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cantidad" required pattern="\d*" oninput="this.value = this.value.replace(/[^0-9]/g, '');" />
+                        <button type="button" class="ml-4 text-red-700 hover:text-red-800" onclick="removeProduct(this)">Eliminar</button>
                     </div>
                 </div>
                 <button type="button" id="add_product" class="text-green-700 hover:text-green-800">Agregar Producto</button>
             </div>
+
             <button type="submit"
                 class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Registrar
                 Venta</button>
@@ -135,109 +130,143 @@
 
 @section('js')
     <script>
-        document.getElementById('buscar_cliente').addEventListener('click', function() {
-        let nro_doc = document.getElementById('nro_doc').value;
-
-        fetch(`/api/clientes/${nro_doc}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.exists) {
-                    document.getElementById('id_cliente').value = data.cliente.id;
-                    document.getElementById('cliente_nombre').innerText = `${data.cliente.nombre} ${data.cliente.apellido}`;
-                    document.getElementById('cliente_email').innerText = data.cliente.email;
-                    document.getElementById('cliente_direccion').innerText = data.cliente.direccion;
-
-                    document.getElementById('cliente_info').classList.remove('hidden');
-                    document.getElementById('nuevo_cliente').classList.add('hidden');
-                } else {
-                    document.getElementById('cliente_info').classList.add('hidden');
-                    document.getElementById('nuevo_cliente').classList.remove('hidden');
+        // Función para actualizar las opciones de productos en todos los selectores
+        function updateProductOptions() {
+            // Recoger todos los productos seleccionados
+            let selectedProducts = [];
+            document.querySelectorAll('.product-select').forEach(function(select) {
+                if (select.value) {
+                    selectedProducts.push(select.value);
                 }
-            })
-            .catch(error => console.error('Error:', error));
+            });
+
+            // Deshabilitar las opciones seleccionadas en otros selects
+            document.querySelectorAll('.product-select').forEach(function(select) {
+                let currentValue = select.value; // Valor actual del select
+                let options = select.querySelectorAll('option');
+
+                options.forEach(function(option) {
+                    if (selectedProducts.includes(option.value) && option.value !== currentValue) {
+                        option.disabled = true; // Deshabilitar opción si ya está seleccionada en otro select
+                    } else {
+                        option.disabled = false; // Habilitar opción si no está seleccionada
+                    }
+                });
+            });
+        }
+
+        // Añadir evento de cambio a los selectores para actualizar las opciones
+        function attachChangeEvent(select) {
+            select.addEventListener('change', function() {
+                updateProductOptions();
+            });
+        }
+
+        document.addEventListener('DOMContentLoaded', function() {
+            // Añadir el evento de cambio a todos los selectores existentes al cargar la página
+            document.querySelectorAll('.product-select').forEach(function(select) {
+                attachChangeEvent(select);
+            });
+
+            // Inicializar las opciones de productos al cargar la página
+            updateProductOptions();
+
+            // Agregar nuevo producto
+            document.getElementById('add_product').addEventListener('click', function() {
+                let productItem = document.querySelector('.producto_item');
+                let newProductItem = productItem.cloneNode(true);
+
+                // Limpiar los valores del nuevo producto clonado
+                let newSelect = newProductItem.querySelector('select');
+                newSelect.value = ''; // Limpiar la selección
+
+                let newInput = newProductItem.querySelector('input');
+                newInput.value = ''; // Limpiar la cantidad
+
+                // Añadir evento al nuevo select
+                attachChangeEvent(newSelect);
+
+                // Añadir el nuevo producto clonado al DOM
+                document.getElementById('productos_container').appendChild(newProductItem);
+
+                // Actualizar las opciones en todos los selectores
+                updateProductOptions();
+            });
         });
 
-        document.getElementById('add_product').addEventListener('click', function() {
-            let productItem = document.querySelector('.producto_item');
-            let newProductItem = productItem.cloneNode(true);
-
-            // Limpiar los valores del nuevo producto clonado
-            let inputs = newProductItem.querySelectorAll('input');
-            inputs.forEach(input => input.value = '');
-
-            let selects = newProductItem.querySelectorAll('select');
-            selects.forEach(select => select.selectedIndex = 0);
-
-            document.getElementById('productos_container').appendChild(newProductItem);
-        });
-
+        // Función para eliminar un producto
         function removeProduct(element) {
             let productItems = document.querySelectorAll('.producto_item');
             if (productItems.length > 1) {
                 element.parentNode.remove();
+                updateProductOptions(); // Actualizar opciones después de eliminar un producto
             }
         }
+
+        document.getElementById('buscar_cliente').addEventListener('click', function() {
+            let nro_doc = document.getElementById('nro_doc').value;
+
+            fetch(`/api/clientes/${nro_doc}`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.exists) {
+                        document.getElementById('id_cliente').value = data.cliente.id;
+                        document.getElementById('cliente_nombre').innerText = `${data.cliente.nombre} ${data.cliente.apellido}`;
+                        document.getElementById('cliente_email').innerText = data.cliente.email;
+                        document.getElementById('cliente_direccion').innerText = data.cliente.direccion;
+
+                        document.getElementById('cliente_info').classList.remove('hidden');
+                        document.getElementById('nuevo_cliente').classList.add('hidden');
+                    } else {
+                        document.getElementById('cliente_info').classList.add('hidden');
+                        document.getElementById('nuevo_cliente').classList.remove('hidden');
+                    }
+                })
+                .catch(error => console.error('Error:', error));
+        });
 
         document.addEventListener('DOMContentLoaded', function() {
             setTimeout(function() {
                 var successMessage = document.getElementById('error-message');
                 if (successMessage) {
-                successMessage.style.transition = 'opacity 0.5s ease';
-                successMessage.style.opacity = '0';
-                setTimeout(function() {
-                    successMessage.remove();
-                }, 500); // Espera el tiempo de la transición para eliminar el elemento
+                    successMessage.style.transition = 'opacity 0.5s ease';
+                    successMessage.style.opacity = '0';
+                    setTimeout(function() {
+                        successMessage.remove();
+                    }, 500); // Espera el tiempo de la transición para eliminar el elemento
                 }
             }, 3000); // 3 segundos antes de empezar a desvanecer
         });
-    </script>
-    <script>
+
         document.getElementById('id_tipo').addEventListener('change', function() {
-        let id_tipo = this.value;
-        let numeracion = '';
+            let id_tipo = this.value;
+            let numeracion = '';
 
-        if (id_tipo == 1) {
-            numeracion = {{ $parametro_1 ? $parametro_1->numeracion : 'null' }};
-        } else if (id_tipo == 2) {
-            numeracion = {{ $parametro_2 ? $parametro_2->numeracion : 'null' }};
-        }
-
-        if (numeracion !== 'null') {
-            numeracion = parseInt(numeracion, 10);
-            if (numeracion < 10) {
-                numeracion='0000' + numeracion;
-            } else if (numeracion < 100) {
-                numeracion='000' + numeracion;
-            } else if (numeracion < 1000) {
-                numeracion='00' + numeracion;
-            } else if (numeracion < 10000) {
-                numeracion='0' + numeracion;
+            if (id_tipo == 1) {
+                numeracion = {{ $parametro_1 ? $parametro_1->numeracion : 'null' }};
+            } else if (id_tipo == 2) {
+                numeracion = {{ $parametro_2 ? $parametro_2->numeracion : 'null' }};
             }
-            document.getElementById('numeracion').value=numeracion;
-            document.getElementById('numeracion_data').value = numeracion;
-        } else {
-            document.getElementById('numeracion').value='No disponible';
-            document.getElementById('numeracion_data').value = '';
-        }
-    });
-    </script>
 
-    <script>
-        document.querySelector('form').addEventListener('submit', function(event) {
-            let productSelects = document.querySelectorAll('select[name="id_producto[]"]');
-            let selectedProducts = [];
-
-            for (let i = 0; i < productSelects.length; i++) {
-                let selectedValue = productSelects[i].value;
-
-                if (selectedProducts.includes(selectedValue)) {
-                    alert('No puedes seleccionar el mismo producto más de una vez.');
-                    event.preventDefault(); // Evita que el formulario se envíe
-                    return false;
-                } else {
-                    selectedProducts.push(selectedValue);
+            if (numeracion !== 'null') {
+                numeracion = parseInt(numeracion, 10);
+                if (numeracion < 10) {
+                    numeracion = '0000' + numeracion;
+                } else if (numeracion < 100) {
+                    numeracion = '000' + numeracion;
+                } else if (numeracion < 1000) {
+                    numeracion = '00' + numeracion;
+                } else if (numeracion < 10000) {
+                    numeracion = '0' + numeracion;
                 }
+                document.getElementById('numeracion').value = numeracion;
+                document.getElementById('numeracion_data').value = numeracion;
+            } else {
+                document.getElementById('numeracion').value = 'No disponible';
+                document.getElementById('numeracion_data').value = '';
             }
         });
     </script>
 @stop
+
+
--- resources/views/ventas/edit.blade.php
+++ resources/views/ventas/edit.blade.php
@@ -108,7 +108,7 @@
                     @foreach($detallesVenta as $detalle)
                         <div class="flex items-center producto_item mb-4">
                             <select name="id_producto[]"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                class="product-select bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 required>
                                 @foreach($productos as $producto)
                                 <option value="{{ $producto->id }}" {{ $detalle->id_producto == $producto->id ? 'selected' : '' }}>
@@ -144,88 +144,140 @@
 
 @section('js')
     <script>
-        document.getElementById('buscar_cliente').addEventListener('click', function() {
-        let nro_doc = document.getElementById('nro_doc').value;
-
-        fetch(`/api/clientes/${nro_doc}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.exists) {
-                    document.getElementById('id_cliente').value = data.cliente.id;
-                    document.getElementById('cliente_nombre').innerText = `${data.cliente.nombre} ${data.cliente.apellido}`;
-                    document.getElementById('cliente_email').innerText = data.cliente.email;
-                    document.getElementById('cliente_direccion').innerText = data.cliente.direccion;
-
-                    document.getElementById('cliente_info').classList.remove('hidden');
-                    document.getElementById('nuevo_cliente').classList.add('hidden');
-                } else {
-                    document.getElementById('cliente_info').classList.add('hidden');
-                    document.getElementById('nuevo_cliente').classList.remove('hidden');
+        // Función para actualizar las opciones de productos en todos los selectores
+        function updateProductOptions() {
+            // Recoger todos los productos seleccionados
+            let selectedProducts = [];
+            document.querySelectorAll('.product-select').forEach(function(select) {
+                if (select.value) {
+                    selectedProducts.push(select.value);
                 }
-            })
-            .catch(error => console.error('Error:', error));
+            });
+
+            // Deshabilitar las opciones seleccionadas en otros selects
+            document.querySelectorAll('.product-select').forEach(function(select) {
+                let currentValue = select.value; // Valor actual del select
+                let options = select.querySelectorAll('option');
+
+                options.forEach(function(option) {
+                    if (selectedProducts.includes(option.value) && option.value !== currentValue) {
+                        option.disabled = true; // Deshabilitar opción si ya está seleccionada en otro select
+                    } else {
+                        option.disabled = false; // Habilitar opción si no está seleccionada
+                    }
+                });
+            });
+        }
+
+        // Añadir evento de cambio a los selectores para actualizar las opciones
+        function attachChangeEvent(select) {
+            select.addEventListener('change', function() {
+                updateProductOptions();
+            });
+        }
+
+        document.addEventListener('DOMContentLoaded', function() {
+            // Añadir el evento de cambio a todos los selectores existentes al cargar la página
+            document.querySelectorAll('.product-select').forEach(function(select) {
+                attachChangeEvent(select);
+            });
+
+            // Inicializar las opciones de productos al cargar la página
+            updateProductOptions();
+
+            // Agregar nuevo producto
+            document.getElementById('add_product').addEventListener('click', function() {
+                let productItem = document.querySelector('.producto_item');
+                let newProductItem = productItem.cloneNode(true);
+
+                // Limpiar los valores del nuevo producto clonado
+                let newSelect = newProductItem.querySelector('select');
+                newSelect.value = ''; // Limpiar la selección
+
+                let newInput = newProductItem.querySelector('input');
+                newInput.value = ''; // Limpiar la cantidad
+
+                // Añadir evento al nuevo select
+                attachChangeEvent(newSelect);
+
+                // Añadir el nuevo producto clonado al DOM
+                document.getElementById('productos_container').appendChild(newProductItem);
+
+                // Actualizar las opciones en todos los selectores
+                updateProductOptions();
+            });
         });
 
-        document.getElementById('add_product').addEventListener('click', function() {
-            let productItem = document.querySelector('.producto_item');
-            let newProductItem = productItem.cloneNode(true);
-
-            // Limpiar los valores del nuevo producto clonado
-            let inputs = newProductItem.querySelectorAll('input');
-            inputs.forEach(input => input.value = '');
-
-            let selects = newProductItem.querySelectorAll('select');
-            selects.forEach(select => select.selectedIndex = 0);
-
-            document.getElementById('productos_container').appendChild(newProductItem);
-        });
-
+        // Función para eliminar un producto
         function removeProduct(element) {
             let productItems = document.querySelectorAll('.producto_item');
             if (productItems.length > 1) {
                 element.parentNode.remove();
+                updateProductOptions(); // Actualizar opciones después de eliminar un producto
             }
         }
+
+        document.getElementById('buscar_cliente').addEventListener('click', function() {
+            let nro_doc = document.getElementById('nro_doc').value;
+
+            fetch(`/api/clientes/${nro_doc}`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.exists) {
+                        document.getElementById('id_cliente').value = data.cliente.id;
+                        document.getElementById('cliente_nombre').innerText = `${data.cliente.nombre} ${data.cliente.apellido}`;
+                        document.getElementById('cliente_email').innerText = data.cliente.email;
+                        document.getElementById('cliente_direccion').innerText = data.cliente.direccion;
+
+                        document.getElementById('cliente_info').classList.remove('hidden');
+                        document.getElementById('nuevo_cliente').classList.add('hidden');
+                    } else {
+                        document.getElementById('cliente_info').classList.add('hidden');
+                        document.getElementById('nuevo_cliente').classList.remove('hidden');
+                    }
+                })
+                .catch(error => console.error('Error:', error));
+        });
 
         document.addEventListener('DOMContentLoaded', function() {
             setTimeout(function() {
                 var successMessage = document.getElementById('error-message');
                 if (successMessage) {
-                successMessage.style.transition = 'opacity 0.5s ease';
-                successMessage.style.opacity = '0';
-                setTimeout(function() {
-                    successMessage.remove();
-                }, 500); // Espera el tiempo de la transición para eliminar el elemento
+                    successMessage.style.transition = 'opacity 0.5s ease';
+                    successMessage.style.opacity = '0';
+                    setTimeout(function() {
+                        successMessage.remove();
+                    }, 500); // Espera el tiempo de la transición para eliminar el elemento
                 }
             }, 3000); // 3 segundos antes de empezar a desvanecer
         });
-    </script>
-    <script>
+
         document.getElementById('id_tipo').addEventListener('change', function() {
-        let id_tipo = this.value;
-        let numeracion = '';
+            let id_tipo = this.value;
+            let numeracion = '';
 
-        if (id_tipo == 1) {
-            numeracion = {{ $parametro_1 ? $parametro_1->numeracion : 'null' }};
-        } else if (id_tipo == 2) {
-            numeracion = {{ $parametro_2 ? $parametro_2->numeracion : 'null' }};
-        }
-
-        if (numeracion !== 'null') {
-            numeracion = parseInt(numeracion, 10);
-            if (numeracion < 10) {
-                numeracion='0000' + numeracion;
-            } else if (numeracion < 100) {
-                numeracion='000' + numeracion;
-            } else if (numeracion < 1000) {
-                numeracion='00' + numeracion;
-            } else if (numeracion < 10000) {
-                numeracion='0' + numeracion;
+            if (id_tipo == 1) {
+                numeracion = {{ $parametro_1 ? $parametro_1->numeracion : 'null' }};
+            } else if (id_tipo == 2) {
+                numeracion = {{ $parametro_2 ? $parametro_2->numeracion : 'null' }};
             }
-            document.getElementById('numeracion').value=numeracion;
-        } else {
-            document.getElementById('numeracion').value='No disponible';
-        }
-    });
+
+            if (numeracion !== 'null') {
+                numeracion = parseInt(numeracion, 10);
+                if (numeracion < 10) {
+                    numeracion = '0000' + numeracion;
+                } else if (numeracion < 100) {
+                    numeracion = '000' + numeracion;
+                } else if (numeracion < 1000) {
+                    numeracion = '00' + numeracion;
+                } else if (numeracion < 10000) {
+                    numeracion = '0' + numeracion;
+                }
+                document.getElementById('numeracion').value = numeracion;
+            } else {
+                document.getElementById('numeracion').value = 'No disponible';
+            }
+        });
     </script>
 @stop
+
